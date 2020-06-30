e3ea80dfee021aa08ba2c34e6ecdc94211eaccf7
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 06:12:16 +0200

    Add router for signup

--- routers/auth.js
+++ routers/auth.js
@@ -4,6 +4,7 @@ const { toJWT } = require('../auth/jwt');
 const Teacher = require('../models').teacher;
 const Student = require('../models').student;
 const Subject = require('../models').subject;
+const { SALT_ROUNDS } = require('../config/constants');
 
 const router = new Router();
 
@@ -51,6 +52,58 @@ router.post('/login', async (req, res, next) => {
     }
   } catch (error) {
     console.log(error);
+    return res.status(400).send({ message: 'Something went wrong, sorry' });
+  }
+});
+
+/*** SIGNUP ***/
+router.post('/signup', async (req, res) => {
+  const { isStudent, name, email, password, teacherId } = req.body;
+  if (!email || !password || !name || !isStudent) {
+    return res
+      .status(400)
+      .send({ message: 'Please provide an email, password and a name' });
+  }
+  try {
+    // STUDENT
+    if (isStudent === 1) {
+      const newStudent = await Student.create({
+        email,
+        password: bcrypt.hashSync(password, SALT_ROUNDS),
+        name,
+        teacherId,
+      });
+
+      delete newStudent.dataValues['password']; // don't send back the password hash
+
+      const token = toJWT({ studentId: newStudent.id });
+
+      const subjects = await Subject.findAll({ attributes: ['id', 'name'] });
+      const message = 'A new account is created for you';
+      res
+        .status(201)
+        .json({ token, ...newStudent.dataValues, subjects, message });
+      // TEACHER
+    } else {
+      const newTeacher = await Teacher.create({
+        email,
+        password: bcrypt.hashSync(password, SALT_ROUNDS),
+        name,
+      });
+
+      delete newTeacher.dataValues['password'];
+
+      const token = toJWT({ teacherId: newTeacher.id });
+
+      res.status(201).json({ token, ...newTeacher.dataValues });
+    }
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      return res
+        .status(400)
+        .send({ message: 'There is an existing account with this email' });
+    }
+
     return res.status(400).send({ message: 'Something went wrong, sorry' });
   }
 });
