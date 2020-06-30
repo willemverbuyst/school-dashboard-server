bdb96aaedc98496691d77cb365b2c776ea16fde5
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 09:08:21 +0200

    Create logout button

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'antd';
 
 export default function LogoutButton() {
-  return <div>Logout</div>;
+  const history = useHistory();
+
+  const handleLogOut = () => {
+    // dispatch logout action student or teacher
+    history.push('/');
+  };
+
+  return <Button onClick={handleLogOut}>Logout</Button>;
 }
