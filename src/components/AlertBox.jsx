a1f06b2f590e8f30bd2834b0eb3f5878485c1614
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 14:06:38 +0200

    Create AlertBox component

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectMessage } from '../store/appState/selectors';
+import { clearMessage } from '../store/appState/actions';
+import { Alert } from 'antd';
 
 export default function AlertBox() {
-  return <div>Display for messages</div>;
+  const message = useSelector(selectMessage);
+  const dispatch = useDispatch();
+  const showMessage = message !== null;
+  if (!showMessage) return null;
+
+  return (
+    <Alert
+      type={message.variant}
+      closable={message.dismissable}
+      onClose={message.dismissable ? () => dispatch(clearMessage()) : null}
+      message={message.text}
+    />
+  );
 }
