741f75d99e71763eedaa68b265bdfbbd167dfbd3
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 11:22:02 +0200

    Add logic to generate the sidebar hc

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import StudentSidebar from '../pages/student/StudentSidebar';
 import TeacherSidebar from '../pages/teacher/TeacherSidebar';
 
-export default function Sidebar() {
-  return <TeacherSidebar />;
+export default function SideBar() {
+  const student = null;
+  const teacher = 'piet';
+  // const teacher = null;
+  // const student = 'sjaak';
+
+  return student ? <StudentSidebar /> : teacher ? <TeacherSidebar /> : null;
 }
