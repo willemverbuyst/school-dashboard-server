d0ac329f0e2c81f407e95ae80e963aa584bf78f6
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 09:05:26 +0200

    Add welcome message to header

--- src/components/BarAtThetop.jsx
+++ src/components/BarAtThetop.jsx
@@ -6,9 +6,21 @@ import { Layout } from 'antd';
 const { Header } = Layout;
 
 export default function BarAtThetop() {
+  const student = null;
+  const teacher = 'Dostojewski';
+
   const renderLoginLogout = () => {
     return <LoginButton />;
   };
+
+  const renderWelcome = () => {
+    return student ? (
+      <div>Welcome {student}!</div>
+    ) : teacher ? (
+      <div>Welcome {teacher}!</div>
+    ) : null;
+  };
+
   return (
     <Header
       style={{
@@ -16,12 +28,13 @@ export default function BarAtThetop() {
         justifyContent: 'space-between',
         alignItems: 'center',
         minHeight: '8vh',
+        color: '#fff',
       }}
     >
       <Link style={{ color: '#fff' }} to="/">
         Dashboard
       </Link>
-      {/* {renderWelcome()} */}
+      {renderWelcome()}
       {renderLoginLogout()}
     </Header>
   );
