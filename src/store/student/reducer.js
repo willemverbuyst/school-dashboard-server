8f1b0b0971cf04c3d7184289a30607c19afbf81b
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 14:40:45 +0200

    Create reducer for login succes

--- src/store/student/reducer.js
+++ src/store/student/reducer.js
@@ -1,1 +1,19 @@
-// reducer student
+import { LOGIN_SUCCESS_STUDENT } from './actions';
+
+const initialState = {
+  token: localStorage.getItem('student_token'),
+  name: null,
+  email: null,
+  id: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS_STUDENT:
+      localStorage.setItem('student_token', action.payload.token);
+      return { ...state, ...action.payload };
+
+    default:
+      return state;
+  }
+};
