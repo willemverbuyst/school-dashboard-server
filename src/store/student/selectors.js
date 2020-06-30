34dc28c94b54f438df8ced9f9f7d92c329641d2c
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 14:50:36 +0200

    Create Selectors

--- src/store/student/selectors.js
+++ src/store/student/selectors.js
@@ -1,1 +1,7 @@
+export const selectStudentName = (state) => state.student.name;
 
+export const selectStudentToken = (state) => state.student.token;
+
+export const selectStudentId = (state) => state.student.id;
+
+export const selectStudentSubjects = (state) => state.student.subjects;
