3b9d3399282d814102fd306b62f6680677f1f32c
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 05:55:24 +0200

    Add router to get all the teachers

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
-
-const { PORT } = require('./config/');
+const loggerMiddleWare = require('morgan');
+const corsMiddleWare = require('cors');
+const { PORT } = require('./config/constants');
+const schoolRouter = require('./routers/general');
 
 const app = express();
+app.use(loggerMiddleWare('dev'));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+app.use(corsMiddleWare());
+
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
+
+app.use('/school', schoolRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
