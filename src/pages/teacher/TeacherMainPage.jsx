a0f48d2a74198054693587e57d8fccb2516d2b1b
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 09:26:46 +0200

    Create a chart component, hard-coded

--- src/pages/teacher/TeacherMainPage.jsx
+++ src/pages/teacher/TeacherMainPage.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Chart from '../../components/charts/Chart';
 
 export default function TeacherMainPage() {
-  return <div>Teacher Main Page</div>;
+  return (
+    <div>
+      Teacher Main Page
+      <Chart />
+    </div>
+  );
 }
