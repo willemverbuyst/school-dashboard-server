cbacb7b9f550656c2c257d40bb21aecf593a627f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 08:22:47 +0200

    Add routing for Home, Login, Signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ChartHomePage from './components/charts/ChartHomePage';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-      <ChartHomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
     </div>
   );
 }
