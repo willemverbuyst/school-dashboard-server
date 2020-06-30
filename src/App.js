db11a0f21d5b39ae3ef3256548903ceb7bf170c3
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-30 11:08:42 +0200

    Add Sidebar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Layout } from 'antd';
 import { Switch, Route } from 'react-router-dom';
 import BarAtTheTop from './components/BarAtThetop';
+import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -20,43 +22,54 @@ function App() {
   return (
     <div>
       <BarAtTheTop />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/students/:studentid" component={StudentMainPage} />
-        <Route
-          exact
-          path="/students/:studentid/subjects/:subjectid"
-          component={StudentSubjectDetails}
-        />
-        <Route
-          exact
-          path="/students/:studentid/subjects/:subjectid/test"
-          component={StudentDoTest}
-        />
-        <Route exact path="/teachers/:teacherid" component={TeacherMainPage} />
-        <Route
-          exact
-          path="/teachers/:teacherid/students/:studentsid"
-          component={TeacherStudentDetails}
-        />
-        <Route
-          exact
-          path="/teachers/:teacherid/subjects/:subjectid"
-          component={TeacherSubjectDetails}
-        />
-        <Route
-          exact
-          path="/teachers/:teacherid/questions/add"
-          component={AddQuestionForm}
-        />
-        <Route
-          exact
-          path="/teachers/:teacherid/questions/list"
-          component={ListOfQuestions}
-        />
-      </Switch>
+      <Layout>
+        <Sidebar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route
+            exact
+            path="/students/:studentid"
+            component={StudentMainPage}
+          />
+          <Route
+            exact
+            path="/students/:studentid/subjects/:subjectid"
+            component={StudentSubjectDetails}
+          />
+          <Route
+            exact
+            path="/students/:studentid/subjects/:subjectid/test"
+            component={StudentDoTest}
+          />
+          <Route
+            exact
+            path="/teachers/:teacherid"
+            component={TeacherMainPage}
+          />
+          <Route
+            exact
+            path="/teachers/:teacherid/students/:studentsid"
+            component={TeacherStudentDetails}
+          />
+          <Route
+            exact
+            path="/teachers/:teacherid/subjects/:subjectid"
+            component={TeacherSubjectDetails}
+          />
+          <Route
+            exact
+            path="/teachers/:teacherid/questions/add"
+            component={AddQuestionForm}
+          />
+          <Route
+            exact
+            path="/teachers/:teacherid/questions/list"
+            component={ListOfQuestions}
+          />
+        </Switch>
+      </Layout>
     </div>
   );
 }
