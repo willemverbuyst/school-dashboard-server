48623293d8befad47594d9d16971ac96d504279f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-01 13:52:01 +0200

    Set up mc test

--- src/pages/student/StudentDoTest.jsx
+++ src/pages/student/StudentDoTest.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectStudentId } from '../../store/student/selectors';
+import { Layout, Button, Row } from 'antd';
 import MultipleChoiceQuestion from '../../components/MultipleChoiceQuestion';
 
+const { Content } = Layout;
+
 export default function StudentDoTest() {
+  const studentId = useSelector(selectStudentId);
+  const { subjectid } = useParams();
+  const history = useHistory();
+
+  const onFinish = () => {
+    console.log('you have finished your test');
+
+    history.push(`/students/${studentId}`);
+  };
+
   return (
-    <div>
-      test for a student
-      <MultipleChoiceQuestion />
-    </div>
+    <Layout>
+      <Layout style={{ padding: '24px', minHeight: '92vh' }}>
+        <Content className="site-layout-background">
+          <Row>
+            hello student #{studentId}, a test for subject #{subjectid} arrrggh
+          </Row>
+          <MultipleChoiceQuestion />
+          <MultipleChoiceQuestion />
+          <MultipleChoiceQuestion />
+          <Button onClick={onFinish}>Finish</Button>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
