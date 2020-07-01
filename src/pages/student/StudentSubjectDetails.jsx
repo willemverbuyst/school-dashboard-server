ae67d85202c08fc8da42800d42857849cacc36aa
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-01 13:43:58 +0200

    Add a button subject details page

--- src/pages/student/StudentSubjectDetails.jsx
+++ src/pages/student/StudentSubjectDetails.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { useParams, useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectStudentId } from '../../store/student/selectors';
+import { Layout, Button, Row } from 'antd';
 
+// import StudentSubjectChart from './StudentSubjectChart';
 const { Content } = Layout;
 
 export default function StudentSubjectDetails() {
+  const studentId = useSelector(selectStudentId);
+  const { subjectid } = useParams();
+  const history = useHistory();
+
+  const goTo = (goto) => {
+    history.push(goto);
+  };
+
   return (
     <Layout>
-      <Layout style={{ padding: '24px', height: '92vh' }}>
+      <Layout style={{ padding: '24px', minHeight: '92vh' }}>
         <Content className="site-layout-background">
-          details for a subject for a student
+          <Row> details for a subject for a student</Row>
+          <Row>
+            <Button
+              onClick={() =>
+                goTo(`/students/${studentId}/subjects/${subjectid}/test`)
+              }
+            >
+              Do a test
+            </Button>
+          </Row>
         </Content>
       </Layout>
     </Layout>
