4b80916f27fc4bc7082dbfa5e01406ab7cb0dc2c
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-01 09:31:03 +0200

    Create action and reducer to fetch questions

--- src/store/tests/actions.js
+++ src/store/tests/actions.js
@@ -1,1 +1,37 @@
-// actions tests
+import axios from 'axios';
+import { apiUrl } from '../../config/constants';
+import { appLoading, appDoneLoading, setMessage } from '../appState/actions';
+
+export const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
+
+export function questionsFetched(questions) {
+  return {
+    type: FETCH_QUESTIONS,
+    payload: questions,
+  };
+}
+
+export function getQuestionsForSubject(id) {
+  return async function thunk(dispatch, getState) {
+    const token = getState().teacher.token;
+    dispatch(appLoading());
+    try {
+      const response = await axios.get(`${apiUrl}/tests/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const questions = response.data;
+
+      dispatch(questionsFetched(questions));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        dispatch(setMessage('danger', true, error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage('danger', true, error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+}
--- src/store/tests/reducer.js
+++ src/store/tests/reducer.js
@@ -1,1 +1,15 @@
-// reducer tests
+import { FETCH_QUESTIONS } from './actions';
+
+const initialState = {
+  all: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_QUESTIONS:
+      return { ...state, all: action.payload };
+
+    default:
+      return state;
+  }
+};
