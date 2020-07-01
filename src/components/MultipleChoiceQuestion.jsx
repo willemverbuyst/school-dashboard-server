57c085024ba380333769192fcda3189b7daae205
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-01 13:39:18 +0200

    Create mc component

--- src/components/MultipleChoiceQuestion.jsx
+++ src/components/MultipleChoiceQuestion.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Radio, Form } from 'antd';
 
 export default function MultipleChoiceQuestion() {
-  return <div>Multiple-choice question card</div>;
+  const [value, setValue] = useState('');
+
+  const radioStyle = {
+    display: 'block',
+    height: '30px',
+    lineHeight: '30px',
+  };
+
+  return (
+    <Form.Item label="Question 1">
+      <br />
+      <Radio.Group onChange={(e) => setValue(e.target.value)} value={value}>
+        <Radio style={radioStyle} value={1}>
+          Option A
+        </Radio>
+        <Radio style={radioStyle} value={2}>
+          Option B
+        </Radio>
+        <Radio style={radioStyle} value={3}>
+          Option C
+        </Radio>
+        <Radio style={radioStyle} value={4}>
+          Option D
+        </Radio>
+      </Radio.Group>
+    </Form.Item>
+  );
 }
--- src/pages/student/StudentDoTest.jsx
+++ src/pages/student/StudentDoTest.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import MultipleChoiceQuestion from '../../components/MultipleChoiceQuestion';
 
 export default function StudentDoTest() {
-  return <div>test for a student</div>;
+  return (
+    <div>
+      test for a student
+      <MultipleChoiceQuestion />
+    </div>
+  );
 }
