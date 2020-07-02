db5e95cf298526a48611b0c6791d85b21201d7dc
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-02 15:54:25 +0200

    Fix bug on teacher with token

--- routers/data.js
+++ routers/data.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const Test = require('../models').test;
-// const Subject = require('../models').subject;
+const Subject = require('../models').subject;
+const Student = require('../models').student;
 const studentAuthMiddleware = require('../auth/studentAuthMiddleware');
+const teacherAuthMiddleware = require('../auth/teacherAuthMiddleware');
 
 const router = new Router();
 
@@ -39,5 +41,27 @@ router.get('/:id', studentAuthMiddleware, async (req, res, next) => {
     return res.status(400).send({ message: 'Something went wrong, sorry' });
   }
 });
+
+// router.get('/students/:id', teacherAuthMiddleware, async (req, res, next) => {
+//   const { id } = req.params;
+//   const teacherId = req.teacher.id;
+
+//   try {
+
+//     const tests = await Test.findAll({
+//       Where: { subjectId: id },
+//       attributes: [
+//         [
+//           Test.sequelize.literal('GROUP(answer1 + answer2 + answer2)'),
+//           'result',
+//         ],
+//       ],
+//     });
+
+//     res.send(tests);
+//   } catch (error) {
+//     return res.status(400).send({ message: 'Something went wrong, sorry' });
+//   }
+// });
 
 module.exports = router;
