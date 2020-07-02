2a59c05d05cf88e15266ffd55cc1d45ec3bb8532
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-02 07:00:02 +0200

    Calculate average

--- src/pages/student/StudentSubjectDetails.jsx
+++ src/pages/student/StudentSubjectDetails.jsx
@@ -30,6 +30,14 @@ export default function StudentSubjectDetails() {
     dispatch(getResultsForSubject(subjectid));
   }, [dispatch, subjectid]);
 
+  const renderAverage = () => {
+    const data = results.map(({ result }) => result);
+    const average = Math.round(
+      (data.reduce((a, b) => a + b, 0) / (data.length * 3)) * 100
+    );
+    console.log(average);
+  };
+
   const renderAmount = () => {
     const subject = subjects.find((subject) => subject.id === subjectid * 1)
       .name;
@@ -38,6 +46,15 @@ export default function StudentSubjectDetails() {
         <Row>You have done</Row>
         <Row>{results.length}</Row>
         <Row>tests for {subject}</Row>
+        <Row>
+          <Button
+            onClick={() =>
+              goTo(`/students/${studentId}/subjects/${subjectid}/test`)
+            }
+          >
+            Do a another test
+          </Button>
+        </Row>
       </>
     );
   };
@@ -55,19 +72,10 @@ export default function StudentSubjectDetails() {
     <Layout>
       <Layout style={{ padding: '24px', minHeight: '92vh' }}>
         <Content className="site-layout-background">
-          <Row> details for a subject for a student</Row>
-          <Row>
-            <Button
-              onClick={() =>
-                goTo(`/students/${studentId}/subjects/${subjectid}/test`)
-              }
-            >
-              Do a test
-            </Button>
-          </Row>
           <div style={{ width: '35vw', height: '35vh' }}>
-            {subjects && results ? renderAmount() : null}
-            {results ? renderChart() : null}
+            {subjects && results[0] ? renderAmount() : null}
+            {results[0] ? renderChart() : null}
+            {results[0] ? renderAverage() : null}
           </div>
         </Content>
       </Layout>
