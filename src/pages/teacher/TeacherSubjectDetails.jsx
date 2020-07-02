acd8694d2c26e8321015a2fb08db2b30bd5aafa1
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-02 20:31:52 +0200

    Add layout and content styling

--- src/pages/teacher/TeacherSubjectDetails.jsx
+++ src/pages/teacher/TeacherSubjectDetails.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Layout } from 'antd';
+const { Content } = Layout;
 
 export default function TeacherSubjectDetails() {
-  return <div>details of subject for teacher</div>;
+  return (
+    <Layout>
+      <Layout style={{ padding: '24px', minHeight: '92vh' }}>
+        <Content className="site-layout-background">
+          details of subject for teacher
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
