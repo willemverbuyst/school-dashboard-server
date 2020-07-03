6896ac682be852d382925b5981bcf6a6269cdd52
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-03 06:48:18 +0200

    Render doughnut chart dynamically

--- src/pages/teacher/TeacherSubjectDetails.jsx
+++ src/pages/teacher/TeacherSubjectDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSubjectForOverview } from '../../store/overviewTeacher/actions';
 import { selectSubjectOverview } from '../../store/overviewTeacher/selectors';
+import { selectTeacherStudents } from '../../store/teacher/selectors';
 import DoughnutChart from '../../components/charts/DoughnutChart';
 import { Layout, Row, Col } from 'antd';
 const { Content } = Layout;
@@ -11,19 +12,21 @@ export default function TeacherSubjectDetails() {
   const dispatch = useDispatch();
   const { subjectid } = useParams();
   const results = useSelector(selectSubjectOverview);
+  const students = useSelector(selectTeacherStudents);
 
   useEffect(() => {
     dispatch(getSubjectForOverview(subjectid));
   }, [dispatch, subjectid]);
 
   const renderCharts = () => {
-    console.log(results);
-    return results.map(({ score }, i) => (
+    return results.map(({ score, studentId }, i) => (
       <Col key={i}>
         <DoughnutChart
           data={[score, 100 - score]}
           color={['#8F1CB8', '#eee']}
-          title={'1'}
+          title={`${
+            students.find((student) => student.id === studentId).name
+          } ${score}%`}
         />
       </Col>
     ));
@@ -33,8 +36,7 @@ export default function TeacherSubjectDetails() {
     <Layout>
       <Layout style={{ padding: '24px', minHeight: '92vh' }}>
         <Content className="site-layout-background">
-          details of subject for teacher
-          <Row>{results ? renderCharts() : null}</Row>
+          <Row>{results && students ? renderCharts() : null}</Row>
         </Content>
       </Layout>
     </Layout>
