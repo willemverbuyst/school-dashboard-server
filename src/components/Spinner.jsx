22c58cbd48b526630faeb41549c0235fe5870a06
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-03 14:40:45 +0200

    Customize spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,5 +2,16 @@ import React from 'react';
 import { Spin } from 'antd';
 
 export default function Spinner() {
-  return <Spin />;
+  return (
+    <Spin
+      style={{
+        transform: 'translate(-50%,-50%) scale(5)',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        opacity: '.3',
+        transition: '.2s ease-in-out',
+      }}
+    />
+  );
 }
