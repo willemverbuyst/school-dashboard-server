cce1c8b2346442c3aa21cb6bb42de268db9dc4ef
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-03 12:22:21 +0200

    Create barchart for average per subject

--- src/pages/teacher/TeacherMainPage.jsx
+++ src/pages/teacher/TeacherMainPage.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import Chart from '../../components/charts/Chart';
+import BarChart from '../../components/charts/BarChart';
 import {
   selectTeacherToken,
   selectTeacherId,
+  selectTeacherSubjects,
 } from '../../store/teacher/selectors';
 import { getMainOverview } from '../../store/overviewTeacher/actions';
-
+import { selectMainOverview } from '../../store/overviewTeacher/selectors';
 import { Layout } from 'antd';
 
 const { Content } = Layout;
@@ -17,6 +18,8 @@ export default function TeacherMainPage() {
   const history = useHistory();
   const token = useSelector(selectTeacherToken);
   const id = useSelector(selectTeacherId);
+  const mainPageData = useSelector(selectMainOverview);
+  const subjects = useSelector(selectTeacherSubjects);
 
   useEffect(() => {
     if (token === null) {
@@ -28,12 +31,27 @@ export default function TeacherMainPage() {
     dispatch(getMainOverview(id));
   }, [dispatch, id]);
 
+  const renderChartsMain = () => {
+    const data = mainPageData.map(({ result }) => result);
+    const color = [];
+    for (let i = 0; i < data.length; i++) color.push('rgb(255, 99, 132)');
+    const labels = subjects.map(({ name }) => name);
+
+    return (
+      <BarChart
+        data={data}
+        color={color}
+        labels={labels}
+        title={`AVERAGES PER SUBJECT`}
+      />
+    );
+  };
+
   return (
     <Layout>
       <Layout style={{ padding: '24px', minHeight: '92vh' }}>
         <Content className="site-layout-background">
-          Teacher Main Page
-          <Chart />
+          {mainPageData && subjects ? renderChartsMain() : null}
         </Content>
       </Layout>
     </Layout>
