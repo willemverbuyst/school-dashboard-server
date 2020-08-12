9a917b1b30b63a808993639c3c74c318636eb166
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-12 20:14:18 +0200

    Add second test for appState-setMessage

--- src/store/appState/test/actions.test.js
+++ src/store/appState/test/actions.test.js
@@ -12,7 +12,7 @@ import {
 
 describe('appState', () => {
   describe('if given set message with text, variant and dismissable', () => {
-    test('should return an object containing type SET_MESSAGE and payload message and variant', () => {
+    test('should return an object containing type SET_MESSAGE and payload variant, dismissable and text', () => {
       const variant = 'test_variant';
       const dismissable = 'test_dismissable';
       const text = 'test_text';
@@ -21,6 +21,18 @@ describe('appState', () => {
         payload: { variant, dismissable, text },
       };
       expect(setMessage(variant, dismissable, text)).toEqual(expected);
+    });
+    test('should return an action object with type SET_MESSAGE and payload the same as message, variant passed', () => {
+      const variant = 'test_variant';
+      const dismissable = 'test_dismissable';
+      const text = 'test_text';
+      const expected = {
+        type: SET_MESSAGE,
+        payload: { variant, dismissable, text },
+      };
+      expect(setMessage(variant, dismissable, text).payload).toEqual(
+        expected.payload
+      );
     });
   });
 });
