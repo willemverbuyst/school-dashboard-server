6b143123acf37f7bd669a764fe9df82eb8b0d080
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-12 21:09:22 +0200

    Add test for clear message

--- src/store/appState/test/reducer.test.js
+++ src/store/appState/test/reducer.test.js
@@ -27,4 +27,13 @@ describe('userReducer', () => {
       expect(newState.loading).toBe(false);
     });
   });
+  describe('when given a CLEAR_MESSAGE action type', () => {
+    test('returns a new state with the message set to null', () => {
+      const action = { type: CLEAR_MESSAGE };
+      const newState = reducer(initialState, action);
+      expect(newState).toEqual({ loading: false, message: null });
+      expect(newState.message).toBeNull;
+      expect(newState.loading).toBe(false);
+    });
+  });
 });
