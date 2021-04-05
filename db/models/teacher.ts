dae9fd21d93b9e7c76bb08b550efff6125febcbf
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-05 11:11:59 +0200

    Update teacher model with ts

---
+++ db/models/teacher.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '.';
+import Student from './student';
+
+interface TeacherAttributes {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+}
+
+interface TeacherCreationAttributes extends Optional<TeacherAttributes, 'id'> {}
+
+interface TeacherInstance
+  extends Model<TeacherAttributes, TeacherCreationAttributes>,
+    TeacherAttributes {
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const Teacher = sequelize.define<TeacherInstance>('Teacher', {
+  id: {
+    allowNull: false,
+    autoIncrement: false,
+    primaryKey: true,
+    type: DataTypes.UUID,
+    unique: true,
+  },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+});
+
+Teacher.hasMany(Student, {
+  sourceKey: 'id',
+  foreignKey: 'studentId',
+  as: 'students',
+});
