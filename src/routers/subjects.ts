c05a2267af198b2577f3ef2dd41eeeed73355be4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-05 16:31:09 +0200

    Create interface for request with body

--- src/routers/subjects.ts
+++ src/routers/subjects.ts
@@ -1,27 +1,32 @@
-import { NextFunction, Request, Router, Response } from 'express';
+import { NextFunction, Router, Response } from 'express';
 // const teacherAuthMiddleware = require('../auth/teacherAuthMiddleware');
 import Subject from '../db/models/subject';
+import { RequestWithBody } from '../interfaces/RequestWithBody';
 
 const router = Router();
 
 // TEACHER post a new subject
-router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-  const { subject } = req.body;
+router.post(
+  '/',
+  async (req: RequestWithBody, res: Response, next: NextFunction) => {
+    const { subject } = req.body;
 
-  if (!subject) {
-    return res.status(400).send({ message: 'Please provide subject' });
-  }
-  try {
-    const newSubject = await Subject.create({
-      name: subject,
-    });
+    if (subject) {
+      try {
+        const newSubject = await Subject.create({
+          name: subject,
+        });
 
-    res
-      .status(201)
-      .send({ newSubject, message: 'You have added a new subject.' });
-  } catch (error) {
-    next(error);
+        res
+          .status(201)
+          .send({ newSubject, message: 'You have added a new subject.' });
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return res.status(400).send({ message: 'Please provide subject' });
+    }
   }
-});
+);
 
 export { router };
