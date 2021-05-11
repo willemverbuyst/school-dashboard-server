4491ade21a50c5344a2676801ce4d9a8e502ebc4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-11 06:50:31 +0200

    Create AppRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { Layout } from 'antd';
 import BarAtTheTop from './components/BarAtThetop';
 import AlertBox from './components/AlertBox';
 import Spinner from './components/Spinner';
 import Sidebar from './components/Sidebar';
-import Home from './pages/home/index';
-import Login from './pages/auth/Login';
-import Signup from './pages/Signup';
-import StudentMainPage from './pages/student/StudentMainPage';
-import StudentDoTest from './pages/student/StudentDoTest';
-import StudentSubjectDetails from './pages/student/StudentSubjectDetails';
-import TeacherMainPage from './pages/teacher/mainpage';
-import TeacherStudentDetails from './pages/teacher/TeacherStudentDetails';
-import TeacherSubjectDetails from './pages/teacher/TeacherSubjectDetails';
-import AddQuestionForm from './pages/teacher/admin/questions/AddQuestionForm';
-import ListOfQuestions from './pages/teacher/admin/questions/ListOfQuestions';
-import AddSubject from './pages/teacher/admin/subjects/AddSubject';
+import AppRouter from './Approuter';
 import { selectAppLoading } from './store/appState/selectors';
 import { getStudentWithStoredToken } from './store/student/actions';
 import { getTeacherWithStoredToken } from './store/teacher/actions';
@@ -39,56 +27,7 @@ function App() {
       <Layout>
         <Sidebar />
         {isLoading ? <Spinner /> : null}
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route
-            exact
-            path="/students/:studentid"
-            component={StudentMainPage}
-          />
-          <Route
-            exact
-            path="/students/:studentid/subjects/:subjectid"
-            component={StudentSubjectDetails}
-          />
-          <Route
-            exact
-            path="/students/:studentid/subjects/:subjectid/test"
-            component={StudentDoTest}
-          />
-          <Route
-            exact
-            path="/teachers/:teacherid"
-            component={TeacherMainPage}
-          />
-          <Route
-            exact
-            path="/teachers/:teacherid/students/:studentid"
-            component={TeacherStudentDetails}
-          />
-          <Route
-            exact
-            path="/teachers/:teacherid/subjects/:subjectid"
-            component={TeacherSubjectDetails}
-          />
-          <Route
-            exact
-            path="/teachers/:teacherid/questions/add"
-            component={AddQuestionForm}
-          />
-          <Route
-            exact
-            path="/teachers/:teacherid/questions/list"
-            component={ListOfQuestions}
-          />
-          <Route
-            exact
-            path="/teachers/:teacherid/subject/add"
-            component={AddSubject}
-          />
-        </Switch>
+        <AppRouter />
       </Layout>
     </div>
   );
---
+++ src/config/routes.ts
@@ -0,0 +1,13 @@
+export const SIGN_UP = '/signup';
+export const LOG_IN = '/login';
+export const HOME = '/';
+
+export const STUDENT_MAIN = '/students/:studentid';
+export const STUDENT_SUBJECTS = '/students/:studentid/subjects/:subjectid';
+export const STUDENT_TEST = '/students/:studentid/subjects/:subjectid/test';
+export const TEACHER_MAIN = '/teachers/:teacherid';
+export const TEACHER_STUDENT = '/teachers/:teacherid/students/:studentid';
+export const TEACHER_SUBJECT = '/teachers/:teacherid/subjects/:subjectid';
+export const TEACHER_QUESTIONS_LIST = '/teachers/:teacherid/questions/list';
+export const TEACHER_QUESTION_ADD = '/teachers/:teacherid/questions/add';
+export const TEACHER_SUBJECT_ADD = '/teachers/:teacherid/subject/add';
