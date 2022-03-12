d25251521725880d5aff55fa088e682997902ace
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-12 06:03:59 +0100

    Add dummy schools and subjects

---
+++ src/prisma/dummyData/schools.ts
@@ -0,0 +1,12 @@
+export const schools = [
+	{
+		id: '29f7727a-11fe-4196-9657-83ded2bd754d',
+		name: 'Compu',
+		location: 'Nerdsville',
+	},
+	{
+		id: '957db6a1-47ed-4d9d-895c-ea7f017f74eb',
+		name: 'Run Faster',
+		location: 'Downhill',
+	},
+]
---
+++ src/prisma/dummyData/subjects.ts
@@ -0,0 +1,14 @@
+export const subjects = [
+	{
+		id: 'e501ac78-a733-44f2-9b1c-df7e94d7a278',
+		name: 'geography',
+	},
+	{
+		id: 'bfed2dc7-c8c9-48c8-a71d-bc93c6bcbbed',
+		name: 'history',
+	},
+	{
+		id: '2748201d-51d2-469b-ab27-fb19005e4bc9',
+		name: 'math',
+	},
+]
