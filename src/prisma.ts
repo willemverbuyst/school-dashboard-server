0ea67b177361c81973a1fc08b484e739bafdc6a9
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-12 06:04:22 +0100

    Update prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,17 +1,48 @@
 import { PrismaClient } from '@prisma/client'
-import { school_1, school_2 } from './prisma/dummyData'
+import { schools } from './prisma/dummyData/schools'
+
+import { subjects } from './prisma/dummyData/subjects'
+import { users } from './prisma/dummyData/users'
 
 const prisma = new PrismaClient()
 
 const createSchools = async (): Promise<void> => {
 	try {
 		await prisma.school.createMany({
-			data: [school_1, school_2],
+			data: schools,
 			skipDuplicates: true,
 		})
 
 		console.log('*******************************************')
-		console.log('Schools created')
+		console.log('Added to the DB: Schools')
+	} catch (error) {
+		console.log(String(error))
+	}
+}
+
+const createSubjects = async (): Promise<void> => {
+	try {
+		await prisma.subject.createMany({
+			data: subjects,
+			skipDuplicates: true,
+		})
+
+		console.log('*******************************************')
+		console.log('Added to the DB: Subjects')
+	} catch (error) {
+		console.log(String(error))
+	}
+}
+
+const createUsers = async (): Promise<void> => {
+	try {
+		await prisma.user.createMany({
+			data: users,
+			skipDuplicates: true,
+		})
+
+		console.log('*******************************************')
+		console.log('Added to the DB: Users')
 	} catch (error) {
 		console.log(String(error))
 	}
@@ -19,11 +50,15 @@ const createSchools = async (): Promise<void> => {
 
 const cleanUpTables = async () => {
 	await prisma.school.deleteMany()
+	await prisma.subject.deleteMany()
+	await prisma.user.deleteMany()
 }
 
 async function main() {
 	await cleanUpTables()
 	await createSchools()
+	await createSubjects()
+	await createUsers()
 }
 
 main()
