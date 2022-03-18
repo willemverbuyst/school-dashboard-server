605e0d54899542a3ff997698f7cd992e93961131
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-18 08:39:23 +0100

    Generate dummy teachers

--- src/prisma/dummyData/teachers.ts
+++ src/prisma/dummyData/teachers.ts
@@ -1,12 +1,10 @@
-export const teachers = [
-	{
-		id: '48765ef6-0799-4cf5-b49e-989a8df0691d',
-		schoolId: '29f7727a-11fe-4196-9657-83ded2bd754d',
-		userId: 'b12036be-ce84-43f4-89af-8517ebeb25d8',
-	},
-	{
-		id: '59f2eb12-66c5-4456-a963-b421367779b6',
-		schoolId: '957db6a1-47ed-4d9d-895c-ea7f017f74eb',
-		userId: '43a74fe7-af76-46c0-9f37-1b74a55b11ab',
-	},
-]
+import { Teacher } from '@prisma/client'
+import { v4 as uuidv4 } from 'uuid'
+import { schools } from './schools'
+import { userTeachers } from './users'
+
+export const teachers: Array<Teacher> = userTeachers.map((teacher, i) => ({
+	id: uuidv4(),
+	schoolId: schools[i].id,
+	userId: teacher.id,
+}))
