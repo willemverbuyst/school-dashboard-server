98210143f7d7feb0b189fd477b7509a9941bc2e8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-18 08:39:12 +0100

    Generate dummy profiles

--- src/prisma/dummyData/profiles.ts
+++ src/prisma/dummyData/profiles.ts
@@ -1,32 +1,20 @@
-export const profiles = [
-	{
-		id: '9f580a5e-a16c-4122-b210-52a6c28292f6',
-		bio: 'Read a book a day and keep the docker away',
-		userId: 'b12036be-ce84-43f4-89af-8517ebeb25d8',
-	},
-	{
-		id: '7a03af2b-91ab-4ca9-8bd0-ae8069d6b403',
-		bio: "Let's rock this place",
-		userId: '43a74fe7-af76-46c0-9f37-1b74a55b11ab',
-	},
-	{
-		id: '77e88bec-f159-4fda-adc6-a05cf9ee875f',
-		bio: 'Run Forest Run',
-		userId: 'aeec7218-6246-4294-a577-571f12959640',
-	},
-	{
-		id: '6aeca8c4-3acd-494a-9a49-3b45751a5100',
-		bio: 'Love sailing',
-		userId: '0988bd08-ace7-4c1d-b0f7-f43014c1cd6e',
-	},
-	{
-		id: 'c399e4cc-0cab-4ba1-b113-f7a8f5d652f0',
-		bio: 'Who',
-		userId: '299cbb11-53e0-4af4-b8c7-fea3cca0ec46',
-	},
-	{
-		id: '074cb880-d2e4-4814-a6e2-4ee63366fd01',
-		bio: 'Not so small',
-		userId: '49b4bc86-bc9e-4279-b180-e604736ec25b',
-	},
+import { Profile } from '@prisma/client'
+import { v4 as uuidv4 } from 'uuid'
+import { userStudents, userTeachers } from './users'
+
+const bios = [
+	'Read a book a day and keep the docker away',
+	"Let's rock this place",
+	'Run Forest Run',
+	'Love sailing',
+	'Who',
+	'Not so small',
 ]
+
+const users = [...userStudents, ...userTeachers]
+
+export const profiles: Array<Profile> = bios.map((bio, i) => ({
+	id: uuidv4(),
+	bio: bio,
+	userId: users[i].id,
+}))
