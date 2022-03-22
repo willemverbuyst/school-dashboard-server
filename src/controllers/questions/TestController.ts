4fdb7951c85d3a209703e0930c4de0539788c8a4
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-22 04:42:51 +0100

    Create test and scores

--- src/controllers/questions/TestController.ts
+++ src/controllers/questions/TestController.ts
@@ -2,7 +2,8 @@ import { NextFunction, Response } from 'express'
 import { RequestWithBody } from '../../interfaces/Requests'
 import { studentAuthMiddleware } from '../../middlewares/studentAuthMiddleware'
 import { getQuestionsForTest } from '../../prisma/queries/questions'
-import { bodyValidator, controller, get, post, use } from '../decorators'
+import { createTest } from '../../prisma/queries/tests'
+import { controller, get, post, use } from '../decorators'
 
 @controller('/questions')
 class TestController {
@@ -36,32 +37,53 @@ class TestController {
 		}
 	}
 
-	// Post the results of his 3q test
-	@post('/questions/3qtest')
-	@bodyValidator('q1', 'q2', 'q3', 'a1', 'a2', 'a3', 'subjectId')
+	@post('/subjects/:id/test')
 	@use(studentAuthMiddleware)
 	async postQuestionForSubject(
 		req: RequestWithBody,
 		res: Response,
 		next: NextFunction
 	): Promise<void> {
-		const { q1, q2, q3, a1, a2, a3, subjectId } = req.body
-		const studentId = req.student.id
+		const {
+			answer1,
+			answer2,
+			answer3,
+			question1,
+			question2,
+			question3,
+			studentId,
+		} = req.body
+		const { id } = req.params
+
+		if (
+			!answer1 ||
+			!answer2 ||
+			!answer3 ||
+			!question1 ||
+			!question2 ||
+			!question3
+		) {
+			res.status(422).send({ message: 'Missing input' })
+			return
+		}
+
+		if (!studentId) {
+			res.status(422).send({ message: 'Student id missing' })
+			return
+		}
 
 		try {
-			// await Test.create({
-			// 	question1: Number(q1),
-			// 	question2: Number(q2),
-			// 	question3: Number(q3),
-			// 	answer1: Number(a1),
-			// 	answer2: Number(a2),
-			// 	answer3: Number(a3),
-			// 	subjectId: Number(subjectId),
-			// 	studentId: Number(studentId),
-			// })
+			const result = await createTest(
+				answer1,
+				answer2,
+				answer3,
+				question1,
+				question2,
+				question3,
+				studentId,
+				id
+			)
 
-			// const result = Number(a1) + Number(a2) + Number(a3)
-			const result = 1
 			res.status(201).send({
 				message: `You have finished your test with a score of ${result}/3`,
 			})
