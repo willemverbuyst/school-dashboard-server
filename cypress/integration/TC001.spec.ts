39afe32263b693e2ab805a45b2b7e2af523d85f1
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-27 06:10:18 +0200

    Test get all schools

--- cypress/integration/TC001.spec.ts
+++ cypress/integration/TC001.spec.ts
@@ -1,10 +1,17 @@
-describe('Mocha’s interface', () => {
-	context(
-		'it provides a way to keep tests easier to read and organized.',
-		() => {
-			it('This is your test case', () => {
-				expect(true).to.eq(true)
+describe('School-daschboard api', () => {
+	context('GET /public/schools', () => {
+		it('should return a list with all schools', () => {
+			cy.request({
+				method: 'GET',
+				url: '/public/schools',
+			}).should(response => {
+				cy.log(JSON.stringify(response.body))
+
+				expect(response.status).to.eq(200)
+				expect(response.body.results).to.eq(4)
+				expect(response.body.data.length).to.be.eq(4)
+				expect(response.body.data[0]).to.have.all.keys('id', 'name', 'location')
 			})
-		}
-	)
+		})
+	})
 })
