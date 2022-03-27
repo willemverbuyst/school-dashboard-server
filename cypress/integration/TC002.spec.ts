9a6c74f258667156799438cc11ac5c75f88365ef
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-27 06:21:37 +0200

    Test get all teachers

---
+++ cypress/integration/TC002.spec.ts
@@ -0,0 +1,23 @@
+describe('School-daschboard api', () => {
+	context('GET /public/teachers', () => {
+		it('should return a list with all teachers including user names', () => {
+			cy.request({
+				method: 'GET',
+				url: '/public/teachers',
+			}).should(response => {
+				cy.log(JSON.stringify(response.body))
+
+				expect(response.status).to.eq(200)
+				expect(response.body.results).to.eq(4)
+				expect(response.body.data.length).to.be.eq(4)
+				expect(response.body.data[0]).to.have.all.keys(
+					'id',
+					'schoolId',
+					'userId',
+					'user'
+				)
+				expect(response.body.data[0].user).to.have.all.keys('userName')
+			})
+		})
+	})
+})
