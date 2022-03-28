de570372cb89a98e1426c3f261259f3e6c561a81
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-28 04:27:57 +0200

    test: get all subjects

---
+++ cypress/integration/TC003_get_allSubjects.spec.ts
@@ -0,0 +1,17 @@
+describe('School-daschboard api', () => {
+	context('GET /public/subjects', () => {
+		it('should return a list with all subjects', () => {
+			cy.request({
+				method: 'GET',
+				url: '/public/subjects',
+			}).should(response => {
+				cy.log(JSON.stringify(response.body))
+
+				expect(response.status).to.eq(200)
+				expect(response.body.results).to.eq(3)
+				expect(response.body.data.length).to.be.eq(3)
+				expect(response.body.data[0]).to.have.all.keys('id', 'name')
+			})
+		})
+	})
+})
