d0e511ee95c1299d02a24db84b0eaa7370619e0d
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-02 06:14:33 +0200

    style: rename and clean up

--- src/prisma/queries/subjects/index.ts
+++ src/prisma/queries/subjects/index.ts
@@ -1,3 +1,3 @@
-export { addSubject } from './addSubject'
+export { addSubject } from './addSubject.query'
 export { getAllSubjects } from './getAllSubjects.query'
 export { getSubject } from './getSubject.query'
