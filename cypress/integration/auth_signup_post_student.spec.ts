69e157f2eb3cf061a8cd9c8ef40d3855a22b00e1
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-18 05:52:40 +0200

    test: update signup student

--- cypress/integration/auth_signup_post_student.spec.ts
+++ cypress/integration/auth_signup_post_student.spec.ts
@@ -1,9 +1,9 @@
 describe('School-daschboard api', () => {
-	context('POST /auth/signup/student', () => {
+	context('POST /auth/signup (student)', () => {
 		it('should return 422 with message - missing input', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {},
 			}).should(response => {
@@ -15,15 +15,16 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing email', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					userName: 'newStudent',
-					password: '123',
-					bio: 'Testing the new student',
 					bsn: '999991632',
+					bio: 'Testing the new student',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -34,15 +35,36 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing userName', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'new@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+				},
+			}).should(response => {
+				expect(response.status).to.eq(422)
+				expect(response.body.message).to.equal('Missing input')
+			})
+		})
+
+		it('should return 422 with message - missing role', function () {
+			cy.request({
+				method: 'POST',
+				url: '/auth/signup',
+				failOnStatusCode: false,
+				body: {
+					bio: 'Testing the new student',
+					bsn: '999991632',
+					email: 'new@student.com',
+					password: '123',
+					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
+					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -53,15 +75,16 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing password', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					userName: 'newStudent',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'new@student.com',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -72,15 +95,16 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing bio', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bsn: '999991632',
+					email: 'new@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -91,15 +115,16 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing bsn', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
+					email: 'new@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -110,15 +135,16 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing school id', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'new@student.com',
+					password: '123',
+					role: 'student',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -129,15 +155,16 @@ describe('School-daschboard api', () => {
 		it('should return 422 with message - missing teacher id', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'new@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(422)
@@ -148,16 +175,17 @@ describe('School-daschboard api', () => {
 		it('should return 400 with message - not unique email', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'test@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'test@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(400)
@@ -171,16 +199,17 @@ describe('School-daschboard api', () => {
 		it('should return 400 with message - invalid school id', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'test@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'test@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '00000000-0000-0000-0000-000000000000',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(400)
@@ -191,16 +220,17 @@ describe('School-daschboard api', () => {
 		it('should return 400 with message - invalid teacher id', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'test@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'test@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '00000000-0000-0000-0000-000000000000',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(400)
@@ -211,21 +241,26 @@ describe('School-daschboard api', () => {
 		it('should return 200 with new student', function () {
 			cy.request({
 				method: 'POST',
-				url: '/auth/signup/student',
+				url: '/auth/signup',
 				failOnStatusCode: false,
 				body: {
-					email: 'new@student.com',
-					userName: 'newStudent',
-					password: '123',
 					bio: 'Testing the new student',
 					bsn: '999991632',
+					email: 'new@student.com',
+					password: '123',
+					role: 'student',
 					schoolId: '8d7016a5-1cca-4da4-8808-a56a25a96bd9',
 					teacherId: '5fd57f7f-37c8-4ce6-9b3c-ef52d66757c3',
+					userName: 'newStudent',
 				},
 			}).should(response => {
 				expect(response.status).to.eq(200)
 				expect(response.body).to.have.all.keys('token', 'data', 'message')
-				expect(response.body.data).to.have.all.keys('subjects', 'user')
+				expect(response.body.data).to.have.all.keys(
+					'subjects',
+					'user',
+					'overview'
+				)
 				expect(response.body.message).to.equal('Welcome')
 			})
 		})
