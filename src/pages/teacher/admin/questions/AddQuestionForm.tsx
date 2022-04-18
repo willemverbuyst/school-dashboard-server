3c824e77fe272e3f1fa058485a0ea27d491a124d
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-18 18:57:56 +0200

    feat: create form to add questions

--- src/pages/teacher/admin/questions/AddQuestionForm.tsx
+++ src/pages/teacher/admin/questions/AddQuestionForm.tsx
@@ -1,7 +1,6 @@
 import { ReactElement, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Layout, Form, Input, Button, Select, Row, Col } from 'antd'
-import FormItem from './FormItem'
 import { useUser } from '../../../../hooks'
 
 const { Content } = Layout
@@ -17,18 +16,11 @@ interface IPostNewQuestion {
 }
 
 const AddQuestionForm = (): ReactElement => {
+	const [form] = Form.useForm()
 	const history = useHistory()
 	const { user } = useUser()
 
 	const subjects = user?.data.subjects.data || []
-	const [newQuestion, setNewQuestion] = useState<IPostNewQuestion>({
-		subject: 1,
-		question: '',
-		answer1: '',
-		answer2: '',
-		answer3: '',
-		answer4: '',
-	})
 
 	useEffect(() => {
 		if (user?.token === null || user?.data.user.role !== 'TEACHER') {
@@ -36,65 +28,47 @@ const AddQuestionForm = (): ReactElement => {
 		}
 	})
 
-	const updateValue = (e: any, value: any) => {
-		console.log(e, value)
-		// setNewQuestion({
-		//   ...newQuestion,
-		//   answer1: e.target.value,
-		// });
+	const handleSubmit = input => {
+		console.log('input :>> ', input)
+		form.resetFields()
 	}
 
 	return (
 		<Content className="site-layout-content" style={{ padding: 90 }}>
 			<Row justify="center" style={{ padding: '24px' }}>
-				{'Select a subject and fill in the data to create a new question.'.toUpperCase()}
+				SELECT A SUBJECT AND CREATE A NEW QUESTION WITH ANSWERS
 			</Row>
 
 			<Row justify="center">
 				<Col style={{ width: 650 }}>
-					<Form name="basic" initialValues={{ remember: true }}>
-						{subjects ? (
-							<Form.Item
-								name="subject"
-								rules={[{ required: true, message: 'Please select a subject' }]}
-							>
-								<Select
-									placeholder="select a subject"
-									value={newQuestion.subject}
-									onChange={e => setNewQuestion({ ...newQuestion, subject: e })}
-								>
-									{subjects.map(({ name, id }, i) => (
-										<Option key={i} value={id}>
-											{name}
-										</Option>
-									))}
-								</Select>
-							</Form.Item>
-						) : null}
-						<FormItem
-							name="Question"
-							message="Please input a question!"
-							value={newQuestion.question}
-							updateValue={updateValue}
-						/>
+					<Form
+						form={form}
+						name="basic"
+						initialValues={{ remember: true }}
+						onFinish={handleSubmit}
+					>
+						<Form.Item
+							name="subject"
+							rules={[{ required: true, message: 'Please select a subject' }]}
+						>
+							<Select placeholder="select a subject">
+								{subjects.map(({ name, id }, i) => (
+									<Option key={i} value={id}>
+										{name}
+									</Option>
+								))}
+							</Select>
+						</Form.Item>
 
 						<Form.Item
-							name="Question"
-							rules={[{ required: true, message: 'Please input a question!' }]}
+							name="question"
+							rules={[{ required: true, message: 'Please enter a question!' }]}
 						>
-							<Input
-								placeholder="Question"
-								value={newQuestion.question}
-								onChange={e =>
-									setNewQuestion({
-										...newQuestion,
-										question: e.target.value,
-									})
-								}
-							/>
+							<Input placeholder="Question" />
 						</Form.Item>
+
 						<Form.Item
-							name="Correct answer"
+							name="correctAnswer"
 							rules={[
 								{
 									required: true,
@@ -102,68 +76,36 @@ const AddQuestionForm = (): ReactElement => {
 								},
 							]}
 						>
-							<Input
-								placeholder="Correct answer"
-								value={newQuestion.answer1}
-								onChange={e =>
-									setNewQuestion({
-										...newQuestion,
-										answer1: e.target.value,
-									})
-								}
-							/>
+							<Input placeholder="Correct answer" />
 						</Form.Item>
+
 						<Form.Item
-							name="Wrong answer #1"
+							name="wrongAnswer1"
 							rules={[
 								{ required: true, message: 'Please input a wrong answer' },
 							]}
 						>
-							<Input
-								placeholder="Wrong answer #1"
-								value={newQuestion.answer2}
-								onChange={e =>
-									setNewQuestion({
-										...newQuestion,
-										answer2: e.target.value,
-									})
-								}
-							/>
+							<Input placeholder="Wrong answer #1" />
 						</Form.Item>
+
 						<Form.Item
-							name="Wrong answer #2"
+							name="wrongAnswer3"
 							rules={[
 								{ required: true, message: 'Please input a wrong answer' },
 							]}
 						>
-							<Input
-								placeholder="Wrong answer #2"
-								value={newQuestion.answer3}
-								onChange={e =>
-									setNewQuestion({
-										...newQuestion,
-										answer3: e.target.value,
-									})
-								}
-							/>
+							<Input placeholder="Wrong answer #2" />
 						</Form.Item>
+
 						<Form.Item
-							name="Wrong answer #3"
+							name="wrongAnswer3"
 							rules={[
 								{ required: true, message: 'Please input a wrong answer' },
 							]}
 						>
-							<Input
-								placeholder="Wrong answer #3"
-								value={newQuestion.answer4}
-								onChange={e =>
-									setNewQuestion({
-										...newQuestion,
-										answer4: e.target.value,
-									})
-								}
-							/>
+							<Input placeholder="Wrong answer #3" />
 						</Form.Item>
+
 						<Form.Item>
 							<Button
 								style={{
@@ -172,7 +114,6 @@ const AddQuestionForm = (): ReactElement => {
 								}}
 								type="primary"
 								htmlType="submit"
-								// onClick={addQuestion}
 							>
 								Add question to the list
 							</Button>
