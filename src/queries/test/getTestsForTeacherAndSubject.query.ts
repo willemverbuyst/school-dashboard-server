619c507dca02d1c5c6dd1be6d757ce8fcb1460f8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-23 04:19:26 +0200

    feat: get tests per subject for teacher

---
+++ src/queries/test/getTestsForTeacherAndSubject.query.ts
@@ -0,0 +1,66 @@
+import { Score } from '@prisma/client'
+import { prismaClient } from '../../prisma'
+import { TestWithSubjectAndScores } from './models'
+
+const groupBy = <T>(
+	array: Array<T>,
+	predicate: (v: T) => string
+): { [key: string]: Array<T> } =>
+	array.reduce((acc, value) => {
+		;(acc[predicate(value)] ||= []).push(value)
+		return acc
+	}, {} as { [key: string]: Array<T> })
+
+const sumScores = (scores: Array<Score>): number =>
+	scores.map(score => score.score).reduce((sum, score) => sum + score, 0)
+
+const reduceScores = (obj: any): any => {
+	return Object.values(obj).map((o: any) => {
+		const numberOfTests = o.length
+		return o.reduce((acc: any, value: any) => {
+			acc.score = (acc.score || 0) + value.score
+			return { ...acc, numberOfTests }
+		})
+	})
+}
+
+const formatTestsForTeacherAndSubject = (
+	tests: Array<TestWithSubjectAndScores>
+): Array<any> => {
+	return tests.map((test: any) => ({
+		subjectId: test.subject.id,
+		subjectName: test.subject.name,
+		score: sumScores(test.scores),
+		studentId: test.student.id,
+		userName: test.student.user.userName,
+	}))
+}
+
+export const getTestForTeacherAndSubject = async (
+	subjectId: string,
+	teacherId: string
+): Promise<any | []> => {
+	const tests = await prismaClient.test.findMany({
+		where: {
+			subjectId,
+			student: { teacherId },
+		},
+		include: {
+			subject: true,
+			scores: true,
+			student: {
+				select: { id: true, user: { select: { id: true, userName: true } } },
+			},
+		},
+	})
+
+	if (tests) {
+		const formattedTests = formatTestsForTeacherAndSubject(tests)
+
+		return reduceScores(
+			groupBy(formattedTests, formattedTest => formattedTest.studentId)
+		)
+	}
+
+	return []
+}
--- src/queries/test/index.ts
+++ src/queries/test/index.ts
@@ -1,11 +1,13 @@
 import { createTest } from './createTest.query'
 import { getAllTestsForTeacher } from './getAllTestsForTeacher.query'
 import { getTestForStudentAndSubject } from './getTestsForStudentAndSubject.query'
+import { getTestForTeacherAndSubject } from './getTestsForTeacherAndSubject.query'
 import { getTestsForStudent } from './getTestsForStudent.query'
 
 export const testQueries = {
 	createTest,
 	getAllTestsForTeacher,
 	getTestForStudentAndSubject,
+	getTestForTeacherAndSubject,
 	getTestsForStudent,
 }
