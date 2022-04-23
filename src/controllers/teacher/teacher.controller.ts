bb235a5355bbe7cf0afdc0825353324674b309a9
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-23 05:36:59 +0200

    feat: get tests per student for teacher

--- src/controllers/teacher/teacher.controller.ts
+++ src/controllers/teacher/teacher.controller.ts
@@ -2,10 +2,11 @@ import { NextFunction, Response } from 'express'
 import { RequestWithBody } from '../../interfaces/Requests'
 import { teacherAuthMiddleware } from '../../middlewares/teacherAuthMiddleware'
 import { teacherQueries } from '../../queries'
-import { getTestForTeacherAndSubject } from '../../queries/test/getTestsForTeacherAndSubject.query'
+import { testQueries } from '../../queries/test'
 import { controller, get, use } from '../decorators'
 
 const { getAllTeachers } = teacherQueries
+const { getTestForTeacherAndStudent, getTestForTeacherAndSubject } = testQueries
 
 @controller('/teachers')
 export class TeacherController {
@@ -40,7 +41,28 @@ export class TeacherController {
 			const tests = await getTestForTeacherAndSubject(subjectId, teacherId)
 
 			if (!tests) {
-				res.send({ message: 'No teachers found' })
+				res.send({ message: 'No tests found' })
+			}
+
+			res.send({ results: tests.length, data: tests })
+		} catch (error) {
+			next(error)
+		}
+	}
+
+	@get('/:teacherId/students/:studentId')
+	@use(teacherAuthMiddleware)
+	async getStudentForTeacher(
+		req: RequestWithBody,
+		res: Response,
+		next: NextFunction
+	): Promise<void> {
+		const { studentId, teacherId } = req.params
+		try {
+			const tests = await getTestForTeacherAndStudent(studentId, teacherId)
+
+			if (!tests) {
+				res.send({ message: 'No tests found' })
 			}
 
 			res.send({ results: tests.length, data: tests })
--- src/queries/test/index.ts
+++ src/queries/test/index.ts
@@ -1,6 +1,7 @@
 import { createTest } from './createTest.query'
 import { getAllTestsForTeacher } from './getAllTestsForTeacher.query'
 import { getTestForStudentAndSubject } from './getTestsForStudentAndSubject.query'
+import { getTestForTeacherAndStudent } from './getTestsForTeacherAndStudent.query'
 import { getTestForTeacherAndSubject } from './getTestsForTeacherAndSubject.query'
 import { getTestsForStudent } from './getTestsForStudent.query'
 
@@ -8,6 +9,7 @@ export const testQueries = {
 	createTest,
 	getAllTestsForTeacher,
 	getTestForStudentAndSubject,
+	getTestForTeacherAndStudent,
 	getTestForTeacherAndSubject,
 	getTestsForStudent,
 }
