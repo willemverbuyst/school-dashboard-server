96ad0552dbe788acd6f6081f75d9b8e1581aaaaa
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-27 06:56:51 +0200

    chore: turn off default options

--- src/react-query/queryClient.ts
+++ src/react-query/queryClient.ts
@@ -1,10 +1,10 @@
 import { QueryClient } from 'react-query'
 
 export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 600000,
-      cacheTime: 900000,
-    },
-  },
+  // defaultOptions: {
+  //   queries: {
+  //     staleTime: 600000,
+  //     cacheTime: 900000,
+  //   },
+  // },
 })
